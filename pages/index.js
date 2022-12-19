d26956b7aac77c6b93249df2804881dbbabb999f
Divine Orji <dpkreativ@gmail.com>
2022-12-19 06:13:04 +0100

    Set up components and layout

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import HomeLayout from '../components/layouts/HomeLayout';
+import Card from '../components/ui/Card';
 
 export default function Home() {
   return (
@@ -9,6 +11,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* Home */}
+      <HomeLayout>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </HomeLayout>
     </>
   );
 }
